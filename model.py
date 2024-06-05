4058fcd25821387ffd93e5ffc0cbf7e51a30bc59
DominikaRadzikowska <radzikowskadominika1@gmail.com>
2024-06-05 17:33:43 +0200

    Initial commit

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
 python
 
-from sklearn.linear_model import Perceptron from sklearn.datasets import load_iris import joblib # Wczytanie danych iris = load_iris() X, y = iris.data, iris.target # Trening modelu model = Perceptron() model.fit(X, y) # Zapisanie modelu joblib.dump(model, 'model.pkl') 
+from sklearn.linear_model import Perceptron from sklearn.datasets import load_iris import joblib 
+# Wczytanie danych 
+iris = load_iris() X, y = iris.data, iris.target 
+# Trening modelu model = Perceptron() model.fit(X, y) 
+# Zapisanie modelu joblib.dump(model, 'model.pkl') 
