67e0906192820458f35205e3605956c89761cb58
DominikaRadzikowska <radzikowskadominika1@gmail.com>
2024-06-05 18:28:58 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,11 @@
 python
 
-from sklearn.linear_model import Perceptron from sklearn.datasets import load_iris import joblib 
-# Wczytanie danych 
+from sklearn.linear_model 
+import Perceptron 
+from sklearn.datasets 
+import load_iris 
+import joblib 
+
 iris = load_iris() X, y = iris.data, iris.target 
-# Trening modelu model = Perceptron() model.fit(X, y) 
-# Zapisanie modelu joblib.dump(model, 'model.pkl') 
+model = Perceptron() model.fit(X, y) 
+oblib.dump(model, 'model.pkl') 
